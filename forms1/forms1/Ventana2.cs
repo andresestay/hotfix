a9569f3998a05982a21f039caef0c791aa494bab
andresestay <andres.estay.e@gmail.com>
2015-01-27 11:32:18 -0300

    Agregar fomrs1 (proyecto c#)

---
+++ forms1/forms1/Ventana2.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace forms1
+{
+    public partial class Ventana2 : Form
+    {
+        public Ventana2()
+        {
+            InitializeComponent();
+        }
+
+        private void Ventana2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int var1;
+            int var2;
+            var1 = Convert.ToInt32(textBox1.Text);
+            var2 = Convert.ToInt32(textBox2.Text);
+            var1 = var1 + var2;
+            label2.Text = Convert.ToString(var1);
+         
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
